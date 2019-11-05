c361b180a6878f666719fb34355ad93322e7e0bf
Arantzazu <arantxa.deam@gmail.com>
2019-11-05 13:40:23 +0100

    3-Arantxa. Home creado para usuario distinto al jefe de estudios.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { HeaderComponent } from '../../../../04-Medicamentos/src/app/components/header/header.component';
+import { HeaderComponent } from './header/header.component';
+
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     IonicModule
