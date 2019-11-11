374f8720dad157fc19649f79e594e4fe5f38dac2
Jonicv6 <jonicv_6@hotmail.com>
2019-11-11 10:31:05 +0100

    5 - Joni

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { HeaderComponent } from 'src/app/components/header/header.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     IonicModule
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  //array de Pagina
+  paginas: Pagina[] = [
+    {
+      imagen: 'assets/img/checklist.png',
+      nombre: 'Listado de visitas',
+      redireccion: '/listado-visitas',
+      descripcion: 'Es un icono de una lista.'
+    },
+    {
+      imagen: 'assets/img/megaphone.png',
+      nombre: 'Comunicado de visita',
+      redireccion: '/comunicado-visitas',
+      descripcion: 'Es un icono de un megáfono.'
+    },
+    {
+      imagen: 'assets/img/attach.png',
+      nombre: 'Documento de visita',
+      redireccion: '/documento-visita',
+      descripcion: 'Es un icono de un documento.'
+    }    
+  ];// fin redirecciones
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface Pagina{
+  imagen: string;
+  nombre: string;
+  redireccion: string;
+  descripcion: string;
 }
