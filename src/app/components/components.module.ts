88ec53741845f84ba5e69e78ab12caa1c50f2911
Jonicv6 <jonicv_6@hotmail.com>
2019-11-11 11:21:36 +0100

    5 - Joni -

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from './header/header.component';
-<<<<<<< HEAD
-=======
-
->>>>>>> b7770456c2bad4a97d5fdd39ce0aefa570d0dce6
 
 
 
