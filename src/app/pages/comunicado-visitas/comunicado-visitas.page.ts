743daaf7dc93c88ebc8d63d4df2d9725eaf1aa0c
Arantzazu <arantxa.deam@gmail.com>
2019-11-20 23:31:23 +0100

    6. Comunicado visitas hecho. Necesitara ser modificado para recabar datos.

--- src/app/pages/comunicado-visitas/comunicado-visitas.page.ts
+++ src/app/pages/comunicado-visitas/comunicado-visitas.page.ts
@@ -7,9 +7,124 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComunicadoVisitasPage implements OnInit {
 
+  // Motivos
+  motivos = ["FTC", "Dual"];
+
+  // Empresas
+  empresas = ["AtSistemas", "Befresh", "Garpo", "Ultimate", "Sierranet","Gades","Asisur"];
+
+  // Fechas
+  fechaVisita: Date = new Date();  
+  fechaMin: String = new Date().toISOString().substring(0,10); // La fecha mínima es la fecha de hoy
+  hoyMes: number = new Date().getMonth();  
+  sigAnio: number = new Date().getFullYear()+1;
+  fechaMax: String = new Date(this.sigAnio, this.hoyMes).toISOString().substr(0,10);
+
+  // Array de horas escogidas
+  horas_elegidas: EleccionHorario[]=[    
+    { // Hora 1
+      inicio: "08:00",
+      fin: "09:00",
+      checkbox: "1",
+      radio_deshabilitado: true,
+      radio_si: "S1",
+      radio_no: "N1"
+    },    
+    { // Hora 2
+      inicio: "09:00",
+      fin: "10:00",
+      checkbox: "2",
+      radio_deshabilitado: true,
+      radio_si: "S2",
+      radio_no: "N2"
+    },    
+    { // Hora 3
+      inicio: "10:00",
+      fin: "11:00",
+      checkbox: "3",
+      radio_deshabilitado: true,
+      radio_si: "S3",
+      radio_no: "N3"
+    },    
+    { // Hora 4
+      inicio: "11:20",
+      fin: "12:20",
+      checkbox: "4",
+      radio_deshabilitado: true,
+      radio_si: "S4",
+      radio_no: "N4"
+    },    
+    { // Hora 5
+      inicio: "12:20",
+      fin: "13:20",
+      checkbox: "5",
+      radio_deshabilitado: true,
+      radio_si: "S5",
+      radio_no: "N5"
+    },    
+    { // Hora 6
+      inicio: "13:20",
+      fin: "14:20",
+      checkbox: "6",
+      radio_deshabilitado: true,
+      radio_si: "S6",
+      radio_no: "N6"
+    } 
+  ];
+
+  datosComunicado: DatosComunicado = {
+    usuario: "",
+    motivo: "",
+    fecha: "",
+    horario: [],
+    empresa1: "",
+    empresa2: ""
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  //Motivo
+  cambioMotivo(seleccionado){
+    console.log('Motivo seleccionado: ', seleccionado);
+  }
+
+  // Fecha
+  cambioFecha(event){
+    console.log("Cambio fecha: ", event.detail.value)
+    console.log('fechaMin: ', this.fechaMin)
+  }
+
+  // Checkbox pulsado
+  pulsadoCheckbox(hora){
+    //console.log("Hora pulsada: " + hora.inicio +"-" + hora.fin)
+    hora.radio_deshabilitado=!hora.radio_deshabilitado;
+  }
+
+  // Enviar pulsado
+  enviarDatos(){
+    this.datosComunicado.usuario=
+  }
+}
+
+// Interfaz de eleccion de horarios
+interface EleccionHorario{
+  inicio: string;
+  fin: string;
+  checkbox: string;
+  radio_deshabilitado: boolean;
+  radio_si: string;
+  radio_no: string;
+}
+
+// Interfaz para almacenar los datos del comunicado
+interface DatosComunicado{
+  usuario: string;
+  motivo: string;
+  fecha: string;
+  horario: string[];
+  empresa1: string;
+  empresa2: string;
 }
